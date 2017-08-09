b4cab8c1bf1258ae72da31c9cf45e9bd64932bfe
Gilles Lasnier <gilles.lasnier@ciele.fr>
2017-08-09 18:06:53 +0200

    modules: baseconfig: fix comments; fix machine hostname; disable time consuming package installation

--- vm-example/puppet/environment/modules/baseconfig/manifests/init.pp
+++ vm-example/puppet/environment/modules/baseconfig/manifests/init.pp
@@ -1,17 +1,24 @@
 # == Class: baseconfig
 #
-# Performs initial configuration tasks for all Vagrant boxes.
+# This class manages initial configuration tasks for all Vagrant boxes.
+#
+# Parameters: none
+#
+# Actions: none
+#
+# Requires: nothing
 #
 class baseconfig {
   exec { 'apt-get update':
     command => '/usr/bin/apt-get update';
   }
 
-  host { 'hostmachine':
+  host { 'vagrant-vm-example-ubuntu-16-04-box':
     ip => '192.168.0.1';
   }
 
-  package { ['tree', 'emacs']:
+#  package { [ 'tree', 'emacs', 'subversion', 'cmake' ]:
+  package { ['tree']:
     ensure => present;
   }
 
