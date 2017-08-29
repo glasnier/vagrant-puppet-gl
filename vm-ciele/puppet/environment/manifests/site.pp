c7c0ac0efb9020f61e059bdb79b78c27944a6855
Gilles Lasnier <gilles.lasnier@gmail.com>
2017-08-29 15:51:43 +0200

    vm-ciele: add admin and simple users support

--- vm-ciele/puppet/environment/manifests/site.pp
+++ vm-ciele/puppet/environment/manifests/site.pp
@@ -1,11 +1,10 @@
-# create a new run stage to ensure certain modules are included first
-stage { 'pre':
-  before => Stage['main']
-}
+# create new run stages to ensure certain modules are included first
+stage { 'pre':  before  => Stage['main'] }
+stage { 'post': require => Stage['main'] }
 
 # add the baseconfig module to the new 'pre' run stage
-class { 'baseconfig':
-  stage => 'pre'
+class {
+  'baseconfig': stage => 'pre';
 }
 
 # set defaults for file ownership/permissions
@@ -15,5 +14,14 @@ File {
   mode  => '0644',
 }
 
-include users
+class { 'users::simple':
+  stage => 'pre',
+  username => 'ciele2'
+}
+
+class { 'users::admin':
+  stage => 'pre',
+  username => 'ciele'
+}
+
 include baseconfig
--- vm-ciele/puppet/environment/modules/users/manifests/init.pp
+++ vm-ciele/puppet/environment/modules/users/manifests/init.pp
@@ -1,33 +1,84 @@
-# == Class: users
+# == Class: users::simple
 #
-# This class manages user configuration for all Vagrant boxes.
+# This class manages simple user configuration for all Vagrant boxes.
 #
-# Parameters: none
+# Parameters:
+#  $username: name of the user to create
+#  $userpasswd: password of the created user (default = 'vagrant')
 #
 # Actions: none
 #
 # Requires: nothing
 #
-class users {
+class users::simple
+(
+  $username = 'cielesimple',
+  $userpasswd = '$6$MLZYHsdS$jnfqHHkGxx1AwXAGI22hVH4mpsP8DEpFTD1htMIsTlx7t.gbFfOIzhC8rl/5dRgPtzpwXkOmBT9vmgRsGHfWh1'
+)
+{
   user {
-    'ciele':
-     ensure  => 'present',
-     managehome => 'true',
-     comment => 'CIELE engineer',
-     groups  => ['adm', 'dialout', 'cdrom', 'sudo', 'dip', 'plugdev', 'lpadmin'],
-     home    => '/home/gil',
-     shell   => '/bin/bash',
-     uid     => '1001',
-     password         => '$6$MLZYHsdS$jnfqHHkGxx1AwXAGI22hVH4mpsP8DEpFTD1htMIsTlx7t.gbFfOIzhC8rl/5dRgPtzpwXkOmBT9vmgRsGHfWh1',
+    $username:
+     name             => "$username",
+     ensure           => 'present',
+     managehome       => 'true',
+     comment          => '$username (admin)',
+     groups  => [ 'dialout', 'plugdev' ],
+     home             => "/home/$username",
+     shell            => '/bin/bash',
+     uid              => '1002',
+     password         => "$userpasswd",
      password_max_age => '99999',
      password_min_age => '0',
   }
 
   file {
-    "/home/ciele":
+    "/home/$username":
      ensure => 'directory',
-     owner  => 'ciele',
-     group  => 'ciele',
+     owner  => "$username",
+     group  => "$username",
+     mode   => '0755'
+  }
+}
+
+# == Class: users::admin
+#
+# This class manages admin user configuration for all Vagrant boxes.
+#
+# Parameters:
+#  $username: name of the user to create
+#  $userpasswd: password of the created user (default = 'vagrant')
+#
+# Actions: none
+#
+# Requires: nothing
+#
+class users::admin
+(
+  $username = 'ciele',
+  $userpasswd = '$6$MLZYHsdS$jnfqHHkGxx1AwXAGI22hVH4mpsP8DEpFTD1htMIsTlx7t.gbFfOIzhC8rl/5dRgPtzpwXkOmBT9vmgRsGHfWh1'
+)
+{
+  user {
+    $username:
+     name             => "$username",
+     ensure           => 'present',
+     managehome       => 'true',
+     comment          => '$username (admin)',
+     groups           => ['adm', 'dialout', 'cdrom', 'sudo', 'dip', 'plugdev', 'lpadmin'  ],
+     home             => "/home/$username",
+     shell            => '/bin/bash',
+     uid              => '1001',
+     #gid              => '1001',
+     password         => "$userpasswd",
+     password_max_age => '99999',
+     password_min_age => '0',
+  }
+
+  file {
+    "/home/$username":
+     ensure => 'directory',
+     owner  => "$username",
+     group  => "$username",
      mode   => '0755'
   }
 }
