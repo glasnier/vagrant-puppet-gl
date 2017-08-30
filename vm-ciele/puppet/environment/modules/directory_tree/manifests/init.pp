b56388f39c0b2040b9e4cc592e7370cee5476356
Gilles Lasnier <gilles.lasnier@gmail.com>
2017-08-30 10:36:11 +0200

    vm-example: puppet: fix puppet language style

--- vm-ciele/puppet/environment/modules/directory_tree/manifests/init.pp
+++ vm-ciele/puppet/environment/modules/directory_tree/manifests/init.pp
@@ -3,44 +3,42 @@
 # This class manages a directory tree creation.
 #
 # Parameters:
-#  $username: name of the user to create
+#  $username: name of the user to create, default value is 'ciele'
 #
 #
 # Actions: none
 #
 # Requires: nothing
 #
-class directory_tree
-(
+class directory_tree (
   $username = 'ciele'
-)
-{
+) {
   # assign a variable to use directories in the resource
   $whisper_dirs = [
-                    "/home/$username/workstation",
-                    "/home/$username/workstation/compilers",
-                    "/home/$username/workstation/documentation",
-                    "/home/$username/workstation/projects",
-                    "/home/$username/workstation/repositories",
-                    "/home/$username/workstation/scripts",
-                    "/home/$username/workstation/tools",
+                    "/home/${username}/workstation",
+                    "/home/${username}/workstation/compilers",
+                    "/home/${username}/workstation/documentation",
+                    "/home/${username}/workstation/projects",
+                    "/home/${username}/workstation/repositories",
+                    "/home/${username}/workstation/scripts",
+                    "/home/${username}/workstation/tools",
                   ]
 
-  file
-  {
-    $whisper_dirs:
-     ensure => 'directory',
-     owner  => "$username",
-     group  => "$username",
-     mode   => '0755',
+  # spell out "magic" iteration
+  $whisper_dirs.each |$path| {
+    file { $path:
+      ensure => directory,
+      owner  => "${username}",
+      group  => "${username}",
+      mode   => '0755',
+    }
   }
 
-  file
-  {
-    "/home/$username/workstation/README":
-      owner => "$username",
-      group => "$username",
-      mode  => '0755',
-      source => 'puppet:///modules/directory_tree/README';
+  file { "/home/${username}/workstation/README":
+    ensure => file,
+    owner  => "${username}",
+    group  => "${username}",
+    mode   => '0755',
+    source => 'puppet:///modules/directory_tree/README';
   }
 }
--- vm-ciele/puppet/environment/modules/users/manifests/init.pp
+++ vm-ciele/puppet/environment/modules/users/manifests/init.pp
@@ -10,33 +10,29 @@
 #
 # Requires: nothing
 #
-class users::simple
-(
+class users::simple (
   $username = 'cielesimple',
   $userpasswd = '$6$MLZYHsdS$jnfqHHkGxx1AwXAGI22hVH4mpsP8DEpFTD1htMIsTlx7t.gbFfOIzhC8rl/5dRgPtzpwXkOmBT9vmgRsGHfWh1'
-)
-{
-  user {
-    $username:
-     name             => "$username",
-     ensure           => 'present',
-     managehome       => 'true',
-     comment          => '$username (admin)',
-     groups  => [ 'dialout', 'plugdev' ],
-     home             => "/home/$username",
-     shell            => '/bin/bash',
-     uid              => '1002',
-     password         => "$userpasswd",
-     password_max_age => '99999',
-     password_min_age => '0',
+) {
+  user { '${username}':
+    name             => "${username}",
+    ensure           => present,
+    managehome       => 'true',
+    comment          => "${username} (admin)",
+    groups           => [ 'dialout', 'plugdev' ],
+    home             => "/home/${username}",
+    shell            => '/bin/bash',
+    uid              => '1002',
+    password         => "${userpasswd}",
+    password_max_age => '99999',
+    password_min_age => '0',
   }
 
-  file {
-    "/home/$username":
-     ensure => 'directory',
-     owner  => "$username",
-     group  => "$username",
-     mode   => '0755'
+  file { '/home/${username}':
+    ensure => directory,
+    owner  => "${username}",
+    group  => "${username}",
+    mode   => '0755'
   }
 }
 
@@ -52,33 +48,29 @@ class users::simple
 #
 # Requires: nothing
 #
-class users::admin
-(
+class users::admin (
   $username = 'ciele',
   $userpasswd = '$6$MLZYHsdS$jnfqHHkGxx1AwXAGI22hVH4mpsP8DEpFTD1htMIsTlx7t.gbFfOIzhC8rl/5dRgPtzpwXkOmBT9vmgRsGHfWh1'
-)
-{
-  user {
-    $username:
-     name             => "$username",
-     ensure           => 'present',
-     managehome       => 'true',
-     comment          => '$username (admin)',
-     groups           => ['adm', 'dialout', 'cdrom', 'sudo', 'dip', 'plugdev', 'lpadmin'  ],
-     home             => "/home/$username",
-     shell            => '/bin/bash',
-     uid              => '1001',
-     #gid              => '1001',
-     password         => "$userpasswd",
-     password_max_age => '99999',
-     password_min_age => '0',
+) {
+  user { '${username}':
+    name             => "${username}",
+    ensure           => present,
+    managehome       => 'true',
+    comment          => '${username} (admin)',
+    groups           => ['adm', 'dialout', 'cdrom', 'sudo', 'dip', 'plugdev', 'lpadmin'  ],
+    home             => "/home/${username}",
+    shell            => '/bin/bash',
+    uid              => '1001',
+    #gid              => '1001',
+    password         => "${userpasswd}",
+    password_max_age => '99999',
+    password_min_age => '0',
   }
 
-  file {
-    "/home/$username":
-     ensure => 'directory',
-     owner  => "$username",
-     group  => "$username",
-     mode   => '0755'
+  file { '/home/${username}':
+    ensure => directory,
+    owner  => "${username}",
+    group  => "${username}",
+    mode   => '0755'
   }
 }
